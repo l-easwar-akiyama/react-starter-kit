8034fadbe3435d3bf3b9ed9345e1edef53ff2f71
Easwar Sivaramakrishnan <easwar.s@inmobi.com>
2019-03-14 14:12:48 +0530

    fix: disabled webpack lodash plugin breaking kebabCase, required further deepdive

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,15 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies, no-unused-vars */
 const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-/* eslint-enable import/no-extraneous-dependencies */
+/* eslint-enable import/no-extraneous-dependencies, no-unused-vars */
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(),
-    new LodashModuleReplacementPlugin(),
     new BundleAnalyzerPlugin({
       analyzerMode: 'disabled',
       openAnalyzer: false,
